b719d8d8b3b2b628d1dc847611abaf012ef30ac0
Tahsin Rahman Chowdhury <tchowdhury29@gmail.com>
2024-11-06 21:08:43 -0600

    Training done

--- Day 12/Namespaces and Scope/task.py
+++ Day 12/Namespaces and Scope/task.py
@@ -8,3 +8,15 @@ def increase_enemies():
 
 increase_enemies()
 print(f"enemies outside function: {enemies}")
+
+
+player_health = 10
+def game():
+    def drink_potion():
+        potion_strength = 2
+        print(potion_strength)
+
+
+#Global Scope
+
+drink_potion()
