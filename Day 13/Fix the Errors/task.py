7d87610c5af2e95ca6ea0b2152f2ab845eee034e
Tahsin Rahman Chowdhury <tchowdhury29@gmail.com>
2024-11-13 22:18:34 -0600

    Number Guessing Game Day12

--- Day 13/Fix the Errors/task.py
+++ Day 13/Fix the Errors/task.py
@@ -1,3 +1,10 @@
-age = int(input("How old are you?"))
-if age > 18:
-print("You can drive at age {age}.")
+try:
+    age = int(input("How old are you?"))
+except ValueError:
+        print("You have typed an invalid number, try again with a numerical response like '15'")
+        age = int(input("How old are you?"))
+
+if age >= 18:
+    print(f"You can drive at age {age}.")
+
+# ValueError: invalid literal for int() with base 10: ' twelve '
